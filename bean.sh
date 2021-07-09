b3c7b9c8eb4480cee77a3a8c5686fdefcd008b46
Bajuna Salehe <bajuna.salehe@gmail.com>
2021-07-09 18:41:28 +0100

    Update bean.sh

--- bean.sh
+++ bean.sh
@@ -2,6 +2,7 @@
 #
 prokka_out_fasta="$1"
 BEAN_PATH="/home/bsalehe/canker_cherry/software/BEAN-2.0/"
+PIPELINE_OUT="/data/scratch/bsalehe/canker_cherry_pipeline_output/"
 #
 #
 # Run BEAN2.0
@@ -12,11 +13,13 @@ perl ${BEAN_PATH}classify.pl $prokka_out_fasta
 cp ${BEAN_PATH}prediction_result.txt ${BEAN_PATH}pipeline_bean2.0_prediction_result_$(date +%Y%m%d%s).txt
 #
 # Copy prediction result new file into final output diectory
-cp ${BEAN_PATH}pipeline_bean2.0_prediction_result_$(date +%Y%m%d%s).txt /data/scratch/bsalehe/canker_cherry_pipeline_output/
+cp ${BEAN_PATH}pipeline_bean2.0_prediction_result_$(date +%Y%m%d%s).txt $PIPELINE_OUT
 #
 # Remove previous BEAN working output directory
-rm -rf ${BEAN_PATH}OUT_work
+#rm -rf ${BEAN_PATH}OUT_work
 #
 # remove previous prediction result file 
 rm ${BEAN_PATH}prediction_result.txt
-
+#
+# remove current prediction result file from current working directory $BEAN_PATH
+rm ${BEAN_PATH}pipeline_bean2.0_prediction_result_$(date +%Y%m%d%s).txt

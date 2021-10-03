eece4fbc45a75f9dba2f9d227ac5f4d76e9522f1
Bajuna Salehe <bajuna.salehe@gmail.com>
2021-10-03 23:10:15 +0100

    Update deepredeff.sh

--- deepredeff.sh
+++ deepredeff.sh
@@ -4,4 +4,4 @@
 FASTAFILE="$1"
 #
 #
-Rscript deepredeff.r $FASTAFILE --slave > /data/scratch/bsalehe/canker_cherry_pipeline_output/deepredeff_result.txt
+Rscript deepredeff.r $FASTAFILE --slave > deepredeff_output

667d1b466b868e7c43fb01d940c9915055fe5861
Bajuna Salehe <bajuna.salehe@gmail.com>
2021-10-03 22:12:58 +0100

    Update genome_cov.sh

--- genome_cov.sh
+++ genome_cov.sh
@@ -4,15 +4,18 @@ R1="$1"
 R2="$2"
 GENOME_SIZE=$3
 
+gsname=$(basename $R1 _L001_R1_trimmed.fastq.gz)
+#gsname=$(basename $R1 _L001_R1_trimmed.fastq.gz)
+
 echo "Usage: genome_size.sh  <file1.fq.gz> <option file2.fq.gz> <genome_size int value>"
 
-./count_nucl.pl -i "$R1" -i "$R2" -g $GENOME_SIZE > coverage.txt
+./count_nucl.pl -i "$R1" -i "$R2" -g $GENOME_SIZE > ${gsname}_coverage.txt
 
-echo "Finished computing coverage $(basename $R1 .fastq.gz)"
+echo "Finished computing coverage $(basename $R1 _L001_R1_trimmed.fastq.gz)"
 
 # Copy coverage output into /data/scratch/bsalehe/
 #
-cp coverage.txt /data/scratch/bsalehe/canker_cherry_pipeline_output/
+cp ${gsname}_coverage.txt /data/scratch/bsalehe/canker_cherry_pipeline_output/
 #
-rm coverage.txt
+rm ${gsname}_coverage.txt
 

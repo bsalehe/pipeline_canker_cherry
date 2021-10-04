498f892cf32810b93a36d333de80bf1cbf06f5d4
Bajuna Salehe <bajuna.salehe@gmail.com>
2021-10-04 12:55:09 +0100

    Update bean.sh

--- bean.sh
+++ bean.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 #
 prokka_out_fasta="$1"
-BEAN_PATH="/home/bsalehe/canker_cherry/software/BEAN-2.0/"
+sname="$2"
+BEAN_PATH="/scratch/software/BEAN2.0/BEAN-2.0/"
 PIPELINE_OUT="/data/scratch/bsalehe/canker_cherry_pipeline_output/"
 #
 #
 # Run BEAN2.0
 #
-perl ${BEAN_PATH}classify.pl $prokka_out_fasta
+cd /scratch/software/BEAN2.0/BEAN-2.0/
+#perl ${BEAN_PATH}classify.pl $prokka_out_fasta
+perl classify.pl $prokka_out_fasta
 #
 # Copy prediction result file into new file
-cp ${BEAN_PATH}prediction_result.txt ${BEAN_PATH}pipeline_bean2.0_prediction_result_$(date +%Y%m%d%s).txt
-#
-# Copy prediction result new file into final output diectory
-cp ${BEAN_PATH}pipeline_bean2.0_prediction_result_$(date +%Y%m%d%s).txt $PIPELINE_OUT
+cp prediction_result.txt /data/scratch/bsalehe/canker_cherry_pipeline_output/${sname}_BEAN2_prediction_result.txt
 #
 # Remove previous BEAN working output directory
 #rm -rf ${BEAN_PATH}OUT_work
 #
-# remove previous prediction result file 
-rm ${BEAN_PATH}prediction_result.txt
+rm -rf OUT_work
 #
-# remove current prediction result file from current working directory $BEAN_PATH
-rm ${BEAN_PATH}pipeline_bean2.0_prediction_result_$(date +%Y%m%d%s).txt
+# remove previous prediction result file 
+#rm ${BEAN_PATH}prediction_result.txt
+rm prediction_result.txt
+#
+cd -

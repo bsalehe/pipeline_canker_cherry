7e6c63a1681dd5bb586a4a7597489cfb63285239
Bajuna Salehe <bajuna.salehe@gmail.com>
2021-10-25 18:38:51 +0100

    Update bean.sh

--- bean.sh
+++ bean.sh
@@ -13,7 +13,7 @@ cd /scratch/software/BEAN2.0/BEAN-2.0/
 perl classify.pl $prokka_out_fasta
 #
 # Copy prediction result file into new file
-mv prediction_result.txt /data/scratch/bsalehe/canker_cherry_pipeline_output/${sname}_BEAN2_prediction_result.txt
+mv prediction_result.txt /data/scratch/bsalehe/canker_cherry_pipeline_output/analysis/Tracy/1/T3/bean2.0/${sname}_BEAN2_prediction_result.txt
 #
 # Remove previous BEAN working output directory
 #rm -rf ${BEAN_PATH}OUT_work

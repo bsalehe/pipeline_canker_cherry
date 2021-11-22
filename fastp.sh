13bd1a90f1adced7bf60252adbc052365786b656
Bajuna Salehe <bajuna.salehe@gmail.com>
2021-11-22 19:03:40 +0000

    fastp.sh

--- fastp.sh
+++ fastp.sh
@@ -2,8 +2,11 @@
 
 R1="$1"
 R2="$2"
-OutDir="$3"
-OutDir1="$4"
+#OutDir="$3"
+#OutDir1="$4"
+
+OutDir=$(basename $R1 001.fastq.gz)trimmed.fastq.gz
+OutDir1=$(basename $R2 001.fastq.gz)trimmed.fastq.gz
 
 fsname=$(basename $OutDir _L001_R1_trimmed.fastq.gz)
 mkdir ${fsname}_fastp_output
@@ -28,17 +31,18 @@ die(){
 
 echo "Start running fastp..."
 
-if [ $# -eq 4 ]; then ## For PE reads
+if [ $# -eq 2 ]; then ## For PE reads
 	fastp -i "$R1" -I "$R2" --correction --trim_front1=10 --trim_front2=10 --trim_tail1=10 --trim_tail2=10 --cut_mean_quality=15 -5 -3 -r -o $OutDir -O $OutDir1 
 #check if the file is one and single-end
-elif [ $# -eq 2 ]; then  ## For SE reads
+elif [ $# -eq 1 ]; then  ## For SE reads
 	fastp -i $R1 -o $OutDir
-else echo "Error: Invalid number of argument. fastp didn't run" 1
+else echo "Error: Invalid number of argument. fastp didn't run" 
+exit 1
 fi
 cp *.html ${fsname}_fastp_output
 cp *.json ${fsname}_fastp_output
 rm *.html *.json
-cp -r ${fsname}_fastp_output /data/scratch/bsalehe/canker_cherry_pipeline_output/assembly_pre-processing/Tracy/1/
+cp -r ${fsname}_fastp_output /data/scratch/bsalehe/canker_cherry_pipeline_output/assembly_pre-processing/Tracy/1/new/
 rm -rf ${fsname}_fastp_output
 #
 #deactivate fastp

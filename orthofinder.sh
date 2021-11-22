0bacb24aa9060c8b2620a421419b124d3f1a3bf1
Bajuna Salehe <bajuna.salehe@gmail.com>
2021-11-22 19:06:51 +0000

    Update orthofinder.sh

--- orthofinder.sh
+++ orthofinder.sh
@@ -9,7 +9,8 @@ WORKDIR="/home/bsalehe/canker_cherry/software/OrthoFinder_source/"
 
 cd $WORKDIR
 
-python3 orthofinder.py -f $INPUTDIR -o /data/scratch/bsalehe/canker_cherry_pipeline_output/analysis/Tracy/1/orthology/OrthoFinder/refgenomes/
+# The output directory should not exist before. Just give the name and Orthofinder will create it for you.
+python3 orthofinder.py -f $INPUTDIR -o /data/scratch/bsalehe/canker_cherry_pipeline_output/analysis/Tracy/1/orthology/OrthoFinder/results_for_combined_3_refgenomes_and_8_samples
 
 cd -
 

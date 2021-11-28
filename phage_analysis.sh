d4870301353c60571a3f92f67806e7e95054a33c
Bajuna Salehe <bajuna.salehe@gmail.com>
2021-11-28 11:00:19 +0000

    Updated phage_analysis.sh

--- phage_analysis.sh
+++ phage_analysis.sh
@@ -13,7 +13,7 @@ export MYCONDAPATH=/home/bsalehe/miniconda3
 source ${MYCONDAPATH}/bin/activate phispy
 
 echo "running phispy "
-PhiSpy.py -o /data/scratch/bsalehe/canker_cherry_pipeline_output/analysis/Tracy/1/phages/phispy/${OutDir} $prokka_out
+PhiSpy.py -o /data/scratch/bsalehe/canker_cherry_pipeline_output/analysis/Tracy/1/phages/phispy/new_run/${OutDir} $prokka_out --randomforest_trees=1000 --phage_genes=0 --keep_dropped_predictions
 echo "phispy finished running"
 
 

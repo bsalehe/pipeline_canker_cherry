c15b92a0f9f5216a93d7b0f4852fb81576547827
Bajuna Salehe <bajuna.salehe@gmail.com>
2022-01-27 15:42:22 +0000

    Add pipeline.sh

---
+++ pipeline.sh
@@ -0,0 +1,167 @@
+#!/bin/bash
+
+#!/usr/bin/env bash
+#SBATCH --partition=long
+#SBATCH --mem=500G
+#SBATCH --cpus-per-task=16
+
+##################################################################################################
+
+# PHASE 1 PIPELINE OF CANKER CHERRY PROJECT: PRE-PROCESSING, ASSEMBLY & ANNOTATION OF GENOMIC READS
+
+###################################################################################################
+
+echo Start running pipeline...
+export PATH=/scratch/software/ncbi-blast-2.11.0+/ncbi-blast-2.11.0+/bin:$PATH
+
+R1="$1"
+R2="$2"
+GENOME_SIZE=$3
+#WORKDIR=$PWD
+#Correction=""
+#Cutoff="off" ## --cov-cutoff
+READSDATADIR="/home/bsalehe/canker_cherry/data/yang/"
+
+QUASTOUTDIR="/data/scratch/bsalehe/canker_cherry_pipeline_output/assembly_pre-processing/Tracy/1/"
+
+PROKKAOUTDIR="/data/scratch/bsalehe/prokka_out/Tracy/refgenomes/"
+
+DEEPREDEFFOUTDIR="/data/scratch/bsalehe/canker_cherry_pipeline_output/analysis/Tracy/1/T3/deepredeff"
+
+################################################################
+#
+# PRE-PROCESSING COVERAGE ANALYSIS: Using FASTP and count_nucl.pl
+#
+################################################################
+
+for file1 in ${READSDATADIR}*_R1_001.fastq.gz; do
+        file2=${file1/R1_/R2_}
+        ./fastp.sh "$file1" "$file2" "$(basename $file1 001.fastq.gz)trimmed.fastq.gz" "$(basename $file2 001.fastq.gz)trimmed.fastq.gz"
+        ./genome_cov.sh "$(basename $file1 001.fastq.gz)trimmed.fastq.gz" "$(basename $file2 001.fastq.gz)trimmed.fastq.gz" 6
+done
+
+##########################################################
+#
+# ASSEMBLY: Using SPAdes
+#
+##########################################################
+
+for file1 in *_R1_trimmed.fastq.gz; do
+        file2=${file1/R1_/R2_}
+        sname=$(basename $file1 _L001_R1_trimmed.fastq.gz)
+        ./SPAdes.sh "$file1" "$file2" "$sname" "off"
+
+##############################################################
+#
+# ASSEMBLY ANALYSIS: Using Quast
+#
+##############################################################
+        ./quast.sh ${sname}/scaffolds.fasta ${sname}_quast_out
+        mv ${sname}_quast_out $QUASTOUTDIR
+
+##############################################################
+#
+# ANNOTATION: Using Prokka
+#
+###############################################################
+
+        #./prokka_ps.sh $Assembly_file
+        ./prokka_ps.sh ${sname}/contigs.fasta
+        #rm -r ${sname}
+done
+
+rm *.gz
+
+#
+#
+#################################################################
+
+# PHASE 2A: EFFECTORS PREDICTION FROM PROKKA ANNOTATION FILES
+
+#################################################################
+# Iterate over directories containing prokka output files
+for prokka_dir in $PROKKAOUTDIR*; do  
+    sample_name=$(basename $prokka_dir);
+    PROKKA_OUT=${prokka_dir}/${sample_name}.faa
+    #file_name=$(basename $PROKKA_OUT .faa)
+    #sample_name=$(dirname $PROKKA_OUT)
+
+#################################################################
+#
+# T3SS: EffectiveT3
+#
+#################################################################
+#
+    ./effective_t3.sh $PROKKA_OUT $sample_name
+#
+#################################################################
+
+#################################################################
+#
+# TXSS: MACSYFINDER
+#
+#################################################################
+#
+    ./macsyf.sh $PROKKA_OUT $sample_name
+#
+#################################################################
+#
+# T3SS: Deepredeff
+#
+#################################################################
+#
+    ./deepredeff.sh $PROKKA_OUT 
+    mv deepredeff_output ${DEEPREDEFFOUTDIR}/${sample_name}_deepredeff_result.csv
+#
+#################################################################
+
+#################################################################
+#
+# T3SS: BEAN2.0
+#
+#################################################################
+#
+#    ./bean.sh $PROKKA_OUT $sample_name
+#
+#################################################################
+#
+#################################################################
+
+# PHASE 2B: PHAGE PREDICTION FROM PROKKA ANNOTATION FILES
+
+#################################################################
+#
+################################################################
+    ./phage_analysis.sh $PROKKA_OUT $sample_name
+#
+##################################################################
+#
+#################################################################
+
+# PHASE 2C: GENOMIC ISLANDS PREDICTION FROM PROKKA ANNOTATION FILES
+
+#################################################################
+#
+#################################################################
+#
+    ./genomicislands.sh $PROKKA_OUT $sample_name
+#
+##################################################################
+
+#################################################################
+
+# PHASE 2D: ORTHOLOGS ANALYSIS FROM PROKKA ANNOTATION FILES
+
+#################################################################
+#
+#################################################################
+#
+# ORTHOLOGUE FINDING: ORTHOFINDER 2.5.4
+#
+#################################################################
+#
+    ./orthofinder.sh $PROKKA_OUT $sample_name
+#
+##################################################################
+
+done

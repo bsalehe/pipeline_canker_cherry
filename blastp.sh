6777d75f4a8c0452c09e1b0c5ca651378b0b2506
Bajuna Salehe <bajuna.salehe@gmail.com>
2022-02-08 18:21:54 +0000

    Add blastp script

---
+++ blastp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+BLASTDBINPUT="/data/scratch/bsalehe/Michelle_data/db_t3se/"
+BLASTPOUT="/data/scratch/bsalehe/canker_cherry_pipeline_output/analysis/Michelle_data/T3/January/blast_confirmed_effectors"
+BLASTQUERYDB="/data/scratch/bsalehe/canker_cherry_pipeline_output/analysis/Michelle_data/T3/January/deepredeff"
+
+export PATH=/scratch/software/ncbi-blast-2.11.0+/ncbi-blast-2.11.0+/bin:$PATH
+
+#makeblastdb -in ${BLASTDBINPUT}Supplemental_Dataset_S4.fasta -dbtype prot
+
+#cd /data/scratch/bsalehe/blast_nrdb/
+  for file in ${BLASTQUERYDB}/*.fasta; do
+    #dname=$(dirname $file)
+    name=$(basename $file .fasta)
+    #blastp -query $file -db ${dirfastafiles}/PROKKA_*.faa -evalue 1e-6 -num_threads 4 -out /data/scratch/bsalehe/canker_cherry_pipeline_outpu>
+    blastp -query $file -db ${BLASTDBINPUT}Supplemental_Dataset_S4.fasta -evalue 1e-6 -num_threads 8 -out ${BLASTPOUT}/${name}_blastp.txt
+  done
+#cd -
+

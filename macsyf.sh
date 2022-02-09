646e3c56ed57372a19b3a7ffeedd2def94008573
Bajuna Salehe <bajuna.salehe@gmail.com>
2022-02-09 18:07:02 +0000

    Update macsyf.sh

--- macsyf.sh
+++ macsyf.sh
@@ -8,10 +8,13 @@ R2="$2"
 #tell bash where to find conda
 export MYCONDAPATH=/home/bsalehe/miniconda3
 
+#MACSYF_OUT="/data/scratch/bsalehe/canker_cherry_pipeline_output/analysis/Tracy/1/T3/January/macsyf/refgenomes"
+MACSYF_OUT="/data/scratch/bsalehe/canker_cherry_pipeline_output/analysis/Michelle_data/T3/January/macsyfinder"
+
 #activate  macsyf
 source ${MYCONDAPATH}/bin/activate macsyf
 
-macsyfinder -m TXSS all --sequence-db $R1 --db-type unordered --models-dir /home/bsalehe/.macsyfinder/data -o /data/scratch/bsalehe/canker_cherry_pipeline_output/analysis/Tracy/1/all_TiSS/macsyfinder/TXSS/new_run/${R2} --min-genes-required T2SS 100  --min-genes-required T1SS 100 --min-genes-required T3SS 100 --min-genes-required T4SS 100 --min-genes-required T5SS 100 --min-genes-required T6SS 100
+macsyfinder -m TXSS all --sequence-db $R1 --db-type unordered --models-dir /home/bsalehe/.macsyfinder/data -o ${MACSYF_OUT}/${R2} --min-genes-required T2SS 100  --min-genes-required T1SS 100 --min-genes-required T3SS 100 --min-genes-required T4SS 100 --min-genes-required T5SS 100 --min-genes-required T6SS 100
 
 conda deactivate
 

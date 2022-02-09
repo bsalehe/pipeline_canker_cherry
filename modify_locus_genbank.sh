14fe80f393fc7b996042aa0cbb479380b8621846
Bajuna Salehe <bajuna.salehe@gmail.com>
2022-02-09 18:10:44 +0000

    Update modify_locus_genbank.sh

--- modify_locus_genbank.sh
+++ modify_locus_genbank.sh
@@ -4,8 +4,9 @@
 
 #########################################################
 
-for dir_with_gb_file in /data/scratch/bsalehe/prokka_out/*; do
-     gb_file=${dir_with_gb_file}/PROKKA_*.gbk
+for dir_with_gb_file in /data/scratch/bsalehe/prokka_out/Tracy/*; do
+     out_dir=$(basename $dir_with_gb_file)
+     gb_file=${dir_with_gb_file}/${out_dir}.gbk
      python3 ./modify_locus_genbank.py $gb_file
 
 done

39134f09901e07da4917ae2614aab2d40420ed66
Bajuna Salehe <bajuna.salehe@gmail.com>
2022-02-15 14:58:05 +0000

    Update blastp.sh

--- blastp.sh
+++ blastp.sh
@@ -2,8 +2,8 @@
 #
 BLASTDBINPUT="/data/scratch/bsalehe/Michelle_data/db_t3se/"
 BLASTPOUT="/data/scratch/bsalehe/canker_cherry_pipeline_output/analysis/Michelle_data/T3/January/blast_confirmed_effectors"
-BLASTQUERYDB="/data/scratch/bsalehe/canker_cherry_pipeline_output/analysis/Michelle_data/T3/January/deepredeff"
-
+#BLASTQUERYDB="/data/scratch/bsalehe/canker_cherry_pipeline_output/analysis/Michelle_data/T3/January/deepredeff"
+BLASTQUERYDB="/data/scratch/bsalehe/canker_cherry_pipeline_output/analysis/Tracy/1/T3/deepredeff"
 export PATH=/scratch/software/ncbi-blast-2.11.0+/ncbi-blast-2.11.0+/bin:$PATH
 
 #makeblastdb -in ${BLASTDBINPUT}Supplemental_Dataset_S4.fasta -dbtype prot
